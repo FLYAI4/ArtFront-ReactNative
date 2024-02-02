7b312d1c0f05c2e347a9b1860bb1fdb1219a5cb4
heehminh <mink141416@gmail.com>
2024-02-02 15:34:36 +0900

    ✨ feat: 바운딩박스 클릭이벤트 - keyword, 설명텍스트 렌더링

--- src/components/Main/FocusPointing/Coordinates.tsx
+++ src/components/Main/FocusPointing/Coordinates.tsx
@@ -1,5 +1,6 @@
-import { View, Text, Image, Dimensions, StyleSheet, SafeAreaView, LayoutChangeEvent, NativeSyntheticEvent, ImageLoadEventData, TouchableOpacity } from 'react-native';
+import { View, Text, Image, Dimensions, SafeAreaView, LayoutChangeEvent, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
+import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
 
 const Coordinates = () => {
   // image resize 
@@ -8,52 +9,58 @@ const Coordinates = () => {
   const originalWidth = 517;
   const originalHeight = 673;
   const screenWidth = Dimensions.get('window').width;
-  const resizeWidth = screenWidth*0.9;
-  const resizeHeight = (screenWidth*originalHeight*0.9) / originalWidth;
+  const screenHeight = Dimensions.get('window').height;
+  const ratio = 0.9;
+  const resizeWidth = screenWidth*ratio;
+  const resizeHeight = (screenWidth*originalHeight*ratio) / originalWidth;
 
   // keyword, context 
-  const [clickIdx, setClickIdx] = useState(0); // keyword 의 idx
   const [keyword, setKeyword] = useState("");
   const [context, setContext] = useState("");
 
   const dict = {
-    "trees": [
-        [
-            134,
-            96,
-            391,
-            466
+    "trees": {
+        "coord": [
+            [
+                134,
+                96,
+                391,
+                566
+            ]
+        ], 
+        "context" : "It features a row of tall, slender poplar trees set against a light blue sky with wispy clouds. The greenery of the trees contrasts with the muted tones of the sky and the golden-green hue of the field in the foreground, suggesting a season like late spring or early summer. Below the trees at the center of the composition is a small white building, perhaps a house or shed, which introduces a human element into the otherwise natural landscape"
+    },
+    "sky": {
+        "coord": [
+            [
+                0,
+                0,
+                517,
+                96
+            ],
         ],
-        "It features a row of tall, slender poplar trees set against a light blue sky with wispy clouds. The greenery of the trees contrasts with the muted tones of the sky and the golden-green hue of the field in the foreground, suggesting a season like late spring or early summer. Below the trees at the center of the composition is a small white building, perhaps a house or shed, which introduces a human element into the otherwise natural landscape"
-    ],
-    "sky": [
-        [
-            0,
-            0,
-            517,
-            96
+        "context": "It features a row of tall, slender poplar trees set against a light blue sky with wispy clouds. The greenery of the trees contrasts with the muted tones of the sky and the golden-green hue of the field in the foreground, suggesting a season like late spring or early summer"
+    },
+    "field": {
+        "coord": [
+            [
+                3,
+                566,
+                514,
+                673
+            ],
         ],
-        "It features a row of tall, slender poplar trees set against a light blue sky with wispy clouds. The greenery of the trees contrasts with the muted tones of the sky and the golden-green hue of the field in the foreground, suggesting a season like late spring or early summer"
-    ],
-    "field": [
-        [
-            3,
-            466,
-            514,
-            673
-        ],
-        "The greenery of the trees contrasts with the muted tones of the sky and the golden-green hue of the field in the foreground, suggesting a season like late spring or early summer"
-    ]
-  }
-
+        "context": "The greenery of the trees contrasts with the muted tones of the sky and the golden-green hue of the field in the foreground, suggesting a season like late spring or early summer"
+    }
+}
 
   const handleImageLayout = (event: LayoutChangeEvent) => {
     const { x, y, width, height } = event.nativeEvent.layout;
     setImageSize({ x, y, width, height });
   }
 
-  const calculateCoordinates = (list: number[]) => {
-    const [x1, y1, x2, y2] = list;
+  const calculateCoordinates = (list: number[][]) => {
+    const [x1, y1, x2, y2] = list[0];
     const x1_ = imageSize.width / originalWidth  * x1 + imageSize.x;
     const y1_ = imageSize.height / originalHeight  * y1 + imageSize.y;
     const x2_ = imageSize.width / originalWidth * x2 + imageSize.x;
@@ -70,24 +77,36 @@ const Coordinates = () => {
     return color;
   };
 
+ const handleClickBounding = (key: string) => {
+   setKeyword(key);
+   setContext(dict[key as keyof typeof dict]['context']);
+ };
+
   const renderBoundingBoxes = () => {
-    return Object.values(dict).map((item: any, index: number) => {
-      const coordinates = calculateCoordinates(item[0]);
-      return (
-        <View key={index} style={{position: 'absolute', borderWidth: 3, borderColor: getRandomColor(), left: coordinates[0], top: coordinates[1], width: coordinates[2] - coordinates[0], height: coordinates[3] - coordinates[1] }} />
-      );
-    });
+    return Object.keys(dict).map((key: string, index: number) => {
+        const coordinates: ReturnType<typeof calculateCoordinates> = calculateCoordinates(dict[key as keyof typeof dict]['coord']);
+        return (
+            <TouchableOpacity 
+                style={{position: 'absolute',left: coordinates[0], top: coordinates[1], width: coordinates[2] - coordinates[0], height: coordinates[3] - coordinates[1], borderWidth: 3, borderColor: getRandomColor() }}
+                onPress={()=> handleClickBounding(key)}>
+                <View key={index}/>
+            </TouchableOpacity>
+        )
+    })
   }
 
   return (
     <SafeAreaView>
-        <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <Text>
-                width: {imageSize.width}px, height: {imageSize.height}px, x: {imageSize.x}, y: {imageSize.y}
-            </Text>
-            <Image source={imageSource} style={{ marginTop:30, width: resizeWidth, height: resizeHeight }} onLayout={handleImageLayout}/>
+        <GestureHandlerRootView>
+        <View style={{width: '100%', height: screenHeight-55, display: 'flex', alignItems: 'center', marginTop: 20, marginBottom: 100,}}>
+            <Image source={imageSource} style={{  width: resizeWidth, height: resizeHeight }} onLayout={handleImageLayout}/>
             {renderBoundingBoxes()}
+            <Text style={{marginTop: 10, marginBottom: 10, fontSize: 24, fontWeight: '600'}} >{keyword}</Text>
+            <ScrollView style={{width: resizeWidth }}>
+                <Text style={{"fontSize":16 }}>{context}</Text>
+            </ScrollView>
         </View>
+        </GestureHandlerRootView>
     </SafeAreaView>
   );
 }
