d63f454efa8eef4f53db9354e779548eed225b84
heehminh <mink141416@gmail.com>
2024-02-15 17:41:57 +0900

    ✨ feat: opencv-loading 무한반복

--- src/components/Main/Loading/OpenCV.tsx
+++ src/components/Main/Loading/OpenCV.tsx
@@ -1,36 +1,43 @@
-import { View, Text } from 'react-native'
-import React, { useEffect} from 'react'
-import GifImage from '@lowkeychat/react-native-gif';
+import { View, Text, Dimensions, useWindowDimensions } from 'react-native'
+import React, { useEffect, useRef, useState} from 'react'
 import Header from '../../Common/Header';
+import FastImage, { Source } from 'react-native-fast-image'
 
 type OpenCVtypes = {
     setIsLoading: (value: boolean) => void
 }
 
 const OpenCV = ({setIsLoading}: OpenCVtypes) => {
-    const loadingGif = 'file:///Users/heehminh/src/ArtFront-ReactNative/src/assets/gif/landscape0.gif';
-  
-    // TODO 서버연결 로딩 컴포넌트
-    // const filePath = storeGifRNFS({ response })
-    // const loadingGif = `file://${filePath}`;
-  
+    const [loadingGif, setLoadingGif] = useState<Source>(require('../../../assets/gif/landscape0.gif'));
+
+    useEffect(()=>{
+        const interval = setInterval(()=> {
+            setLoadingGif(require("../../../assets/image/loading-whitebox.png"))
+            setLoadingGif(require('../../../assets/gif/landscape0.gif'))
+        }, 20000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [])
+
     useEffect(()=>{
       const timer = setTimeout(()=>{
         setIsLoading(false);
-      }, 50000);
+      }, 30000);
   
       return () => clearTimeout(timer);
     }, []);
-
 
   return (
     <View style={{backgroundColor:'white'}}>
         <Header nextPage='MainScreen' />
         <View style={{"backgroundColor": 'white', width: "100%", height: "100%", display: 'flex', marginTop:150,  alignItems: 'center'}}>
-          <GifImage source={{uri: loadingGif}} style={{zIndex:1, width: 400, height: 400 }} resizeMode={'contain'} />
-          <Text style={{marginTop: 30}}>그림의 생명을 불어넣는중..</Text>
+          <FastImage source={ loadingGif as Source } style={{zIndex:1, width: 400, height: 400 }} resizeMode={FastImage.resizeMode.contain}/>
+          
+          <Text style={{marginTop: 30}}>그림에 생명을 불어넣는 중..</Text>
         </View>
-        </View>
+    </View>
   )
 }
 
