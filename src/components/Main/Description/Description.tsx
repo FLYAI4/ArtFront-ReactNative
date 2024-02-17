913215961ecb01a6ae5eb1a4e9a431efa5c15b11
heehminh <mink141416@gmail.com>
2024-02-17 22:30:32 +0900

    ✨ feat: 메인콘텐츠 나레이션 TTS - 재생, 일시중지

--- src/components/Main/Description/Description.tsx
+++ src/components/Main/Description/Description.tsx
@@ -1,9 +1,10 @@
-import { Image, View, Text, Dimensions, LayoutChangeEvent, SafeAreaView, TouchableWithoutFeedback, NativeSyntheticEvent, NativeScrollEvent } from 'react-native'
-import React, { useState } from 'react'
+import { Image, Button, View, Text, Dimensions, LayoutChangeEvent, SafeAreaView, TouchableWithoutFeedback, NativeSyntheticEvent, NativeScrollEvent, TouchableOpacity } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
 import { heightSelector, uriSelector, widthSelector } from '../../../recoil/selector';
 import { useRecoilValue } from 'recoil';
 import { treeContent } from '../../../constants/imageInfo';
+import SoundPlayer from 'react-native-sound-player'
 
 const Description = () => {
   const uri = useRecoilValue(uriSelector);
@@ -17,12 +18,53 @@ const Description = () => {
   const ratio = 0.9;
   const resizeWidth = screenWidth*ratio;
   const resizeHeight = (screenWidth*originalHeight*ratio) / originalWidth;
+  const [play, setPlay] = useState(false);
+
+  useEffect(()=>{
+    const playSound = async () => {
+      try {
+        await SoundPlayer.loadSoundFile('main', 'mp3')
+      } catch (e) {
+        console.log('사운드 파일 재생 오류', e)
+      }
+    }
+
+    playSound();
+    
+    SoundPlayer.addEventListener('FinishedPlaying', ({ success }) => {
+      if (success) {
+        SoundPlayer.pause();
+      }
+    });
+
+    SoundPlayer.addEventListener('FinishedLoading', ({ success }) => {
+      console.log('finished loading', success)
+    });
+
+    SoundPlayer.addEventListener('FinishedLoadingFile', ({ success }) => {
+      console.log('finished loading file', success)
+    });
+  
+    return () => {
+      SoundPlayer.pause();
+    };
+  }, []);
+
+  const onClickButton = () => {
+    !play ? SoundPlayer.play() : SoundPlayer.pause()
+    setPlay(!play)
+  }
 
   return (
     <SafeAreaView>  
       <GestureHandlerRootView>
         <View style={{ width: '100%', display: 'flex', alignItems: 'center', marginTop: 20, }}>
           <Image source={{ uri: uri }} style={{width: resizeWidth, height: resizeHeight }} />
+          <View style={{display: 'flex', flexDirection: 'row'}}>
+            <TouchableOpacity>
+              <Button title={play ? '일시중지' : '재생'} onPress={onClickButton}/>
+            </TouchableOpacity>
+          </View>
           <ScrollView 
             style={{ marginTop: 10, width: resizeWidth, height: 220 }} 
             showsVerticalScrollIndicator={false}
--- src/components/Main/Loading/Splash.tsx
+++ src/components/Main/Loading/Splash.tsx
@@ -18,9 +18,6 @@ const Splash = ({setSplash}: SplashProps) => {
     }, []);
 
   return (
-    // <View>
-    //     <Text>sasdgsgasdg</Text>
-    // </View>
     <Video source={splashScreen} style={{ position: 'absolute', width: '100%', height: '100%', top: 0, bottom: 0, left: 0, right: 0}} />
   )
 }
