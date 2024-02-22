f7c8b035037f070f9e8aec3fb3c6dac184c1445b
heehminh <mink141416@gmail.com>
2024-02-22 11:18:38 +0900

    🔧 chore: app name 변경

--- ios/acentTests/acentTests.m
+++ ios/acentTests/acentTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface artTests : XCTestCase
+@interface acentTests : XCTestCase
 
 @end
 
-@implementation artTests
+@implementation acentTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
