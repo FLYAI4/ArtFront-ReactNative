f4dda8575cd32cb9ba6fb642be44a22a37edc055
heehminh <mink141416@gmail.com>
2024-02-23 14:48:34 +0900

    💅 design: loading gif

--- src/components/Main/Loading/OpenCV.tsx
+++ src/components/Main/Loading/OpenCV.tsx
@@ -2,24 +2,16 @@ import { View, Text, Dimensions, useWindowDimensions } from 'react-native'
 import React, { useEffect, useRef, useState} from 'react'
 // import Header from '../../Common/Header';
 import FastImage, { Source } from 'react-native-fast-image'
+import { width, height } from '../../../constants/imageInfo'
+import AppText from '../../Common/Text/AppText'
+import theme from '../../../../theme'
 
 type OpenCVtypes = {
     setIsLoading: (value: boolean) => void
 }
 
 const OpenCV = ({setIsLoading}: OpenCVtypes) => {
-    const [loadingGif, setLoadingGif] = useState<Source>(require('../../../assets/gif/landscape0.gif'));
-
-    useEffect(()=>{
-        const interval = setInterval(()=> {
-            setLoadingGif(require("../../../assets/image/loading-whitebox.png"))
-            setLoadingGif(require('../../../assets/gif/landscape0.gif'))
-        }, 20000);
-
-        return () => {
-            clearInterval(interval);
-        }
-    }, [])
+    const [loadingGif, setLoadingGif] = useState<Source>(require('../../../assets/gif/opencv1.gif'));
 
     useEffect(()=>{
       const timer = setTimeout(()=>{
@@ -30,12 +22,11 @@ const OpenCV = ({setIsLoading}: OpenCVtypes) => {
     }, []);
 
   return (
-    <View style={{backgroundColor:'white', display: 'flex',  width: '100%', height: '100%'}}>
-        {/* <Header nextPage='HomeScreen' /> */}
-        <View style={{paddingBottom: 75, backgroundColor: 'white', width: "100%", height: "100%", display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <FastImage source={ loadingGif as Source } style={{zIndex:1, width: 400, height: 400 }} resizeMode={FastImage.resizeMode.contain}/>
-          <Text style={{marginTop: 30}}>그림에 생명을 불어넣는 중..</Text>
+    <View style={{ display: 'flex',  flexDirection:'column', width: '100%', height: '100%'}}>
+        <View style={{ backgroundColor: theme.olive, width: "100%", height: "100%", display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
+          <FastImage source={ loadingGif as Source } style={{zIndex:1, width: width, height: height }} resizeMode={FastImage.resizeMode.contain} />
         </View>
+        <AppText style={{ position:'absolute', fontWeight:'600', left: 0, right: 0, bottom:30, width:'100%', textAlign: 'center', fontSize: 20, color: theme.backgroundWhite}}>그림에 생명을 불어넣는 중..</AppText>
     </View>
   )
 }
