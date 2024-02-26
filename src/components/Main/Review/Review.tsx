cf669970e9d703224e6652d57cff388efab058e4
heehminh <mink141416@gmail.com>
2024-02-26 14:40:20 +0900

    ✨ feat: 리뷰작성 API

--- src/components/Main/Review/Review.tsx
+++ src/components/Main/Review/Review.tsx
@@ -9,6 +9,9 @@ import { heightSelector, uriSelector, widthSelector } from '../../../recoil/sele
 import { useRecoilValue } from 'recoil';
 import { ParamListBase, useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
+import { useMutation } from 'react-query'
+import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const Review = () => {
     const uri = useRecoilValue(uriSelector);
@@ -21,20 +24,50 @@ const Review = () => {
     const resizeHeight = (screenWidth*originalHeight*ratio) / originalWidth;
 
     const [like, setLike] = useState(true);
-    const [comment, setComment] = useState('');
+    const [content, setContent] = useState('');
 
     const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
+
+    const reviewMutation = useMutation(async () => {
+      const data = {
+        like_status: like,
+        review_content: content
+      }
+      const userData = await AsyncStorage.getItem('userData');
+      if (userData !== null) {
+        const userInfo = JSON.parse(userData);
+        const response = await axios.post(`${process.env.BASE_URL}/user/content/review`, data, {
+          headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'id': userInfo.id,
+            'generated-id': '4.jpg',
+            'token': userInfo.token
+          }
+        })
+        return response.data;
+      }
+    });
+
+  const handleSubmit = async () => {
+    const response = await reviewMutation.mutateAsync();
+
+    if (response.data.id) {
+      Alert.alert('Acent를 통해 작품을 감상해주셔서 감사합니다. 다른 작품도 감상해보세요!')
+      navigation.push('HomeScreen');
+    }
+    
+  }
 
   return (
     <SafeAreaView>
         <ScrollView style={{width: '100%', height: '100%', display: 'flex', flexDirection: 'column'}}>
             <ReviewImage uri={uri} resizeWidth={resizeWidth} resizeHeight={resizeHeight}/>
             <ReviewButton like={like} setLike={setLike} />
-            <ReviewText comment={comment} setComment={setComment}/>
+            <ReviewText content={content} setContent={setContent}/>
             <TouchableOpacity 
                 style={{ marginLeft: 20, marginRight: 20, backgroundColor: theme.olive, marginBottom: 20, borderRadius: 20}}
-                // onPress={()=>Alert.alert(`좋아요 버튼을 눌렀나요? ${like} ${comment}` )}>
-                onPress={()=>navigation.push('HomeScreen')}>
+                onPress={handleSubmit}>
                 <AppText style={{ padding: 15, fontWeight: '800', fontSize: 18, color: theme.backgroundWhite,  textAlign: 'center'}}>소감 남기기</AppText>
             </TouchableOpacity>
         </ScrollView>
